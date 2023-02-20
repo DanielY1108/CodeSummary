05ccae760dbb1e7c1bc90c1f9882c3a3ea3d2715
DanielY1108 <scarlet040@gmail.com>
2023-02-20 21:29:38 +0900

    유닛 테스트 간단 정리

--- UnitTestCalc-DemoTests/UnitTestCalc_DemoTests.swift
+++ UnitTestCalc-DemoTests/UnitTestCalc_DemoTests.swift
@@ -39,27 +39,59 @@ final class UnitTestCalc_DemoTests: XCTestCase {
     
 }
 
-//    override func setUpWithError() throws {
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//    }
+
+//유닛 테스트에서 아래와 같은 개념들이 중요합니다.
 //
-//    override func tearDownWithError() throws {
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//    }
+//1. 속도 (Speed)
 //
-//    func testExample() throws {
-//        // This is an example of a functional test case.
-//        // Use XCTAssert and related functions to verify your tests produce the correct results.
-//        // Any test you write for XCTest can be annotated as throws and async.
-//        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-//        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-//    }
+//유닛 테스트는 개발자가 코드 변경사항을 검증하는 데 사용되므로 빠른 실행 속도가 필요합니다. 느린 테스트는 개발자가 코드를 수정하고 결과를 확인하기까지 시간이 많이 걸리므로 생산성을 저하시킵니다. 따라서 유닛 테스트에서는 빠른 속도로 테스트를 수행할 수 있도록 설계해야 합니다.
 //
-//    func testPerformanceExample() throws {
-//        // This is an example of a performance test case.
-//        measure {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
+//1. 자동화 (Automation)
 //
-//}
+//자동화란, 테스트를 실행하고 결과를 확인하는 모든 과정을 자동으로 처리하는 것을 의미합니다. 수동으로 테스트를 수행하는 것보다 자동화된 테스트를 수행하면 시간과 비용을 절약할 수 있습니다. 또한 자동화된 테스트는 개발자가 코드를 변경하거나 기능을 추가할 때마다 쉽게 실행할 수 있어서 버그를 신속하게 찾을 수 있습니다.
+//
+//1. 독립적 (independent)
+//
+//유닛 테스트는 각각의 테스트가 서로 독립적으로 실행될 수 있어야 합니다. 이것은 특정 테스트의 결과가 다른 테스트에 영향을 미치지 않아야 한다는 것을 의미합니다. 즉, 의존성을 최소화하고 각각의 테스트 케이스가 독립적으로 실행될 수 있도록 작성하는 것이 중요합니다.  이러한 방식으로 작성된 유닛 테스트는 코드 변경에 대해 신속하게 반응할 수 있으며, 테스트 결과에 대한 신뢰도를 높일 수 있습니다.
+//
+//1. 반복적 (Repeatable)
+//
+//유닛 테스트는 반복적으로 실행될 수 있어야 합니다. 동일한 입력값에 대해서는 항상 동일한 결과를 보여야 하며, 실행 순서나 실행환경이 달라져도 동일한 결과를 보여주어야 합니다. 이러한 특징은 테스트의 신뢰성을 높일 수 있으며, 변경사항에 대한 검증을 보장합니다.
+
+
+//XCTest 프레임워크에서 제공하는 XCTAssert 메서드는 유닛 테스트에서 결과 값을 검증하기 위한 방법을 제공합니다. 아래는 주요한 몇 가지 Assert 메서드입니다.
+//
+//1. XCTAssertEqual(expected: T, expression: T, message: String)
+//- 두 값이 같은지 확인합니다.
+//- expected: 예상 값
+//- expression: 테스트 대상 실제 값
+//- message: 실패 시 출력할 메시지
+//1. XCTAssertNotEqual(first: T, second: T, message: String)
+//- 두 값이 다른지 확인합니다.
+//- first: 첫 번째 값
+//- second: 두 번째 값
+//- message: 실패 시 출력할 메시지
+//1. XCTAssertTrue(expression: Bool, message: String)
+//- 조건이 참인지 확인합니다.
+//- expression: 테스트 대상 조건
+//- message: 실패 시 출력할 메시지
+//1. XCTAssertFalse(expression: Bool, message: String)
+//- 조건이 거짓인지 확인합니다.
+//- expression: 테스트 대상 조건
+//- message: 실패 시 출력할 메시지
+//1. XCTAssertNil(expression: Any?, message: String)
+//- 값이 nil인지 확인합니다.
+//- expression: 테스트 대상 값
+//- message: 실패 시 출력할 메시지
+//1. XCTAssertNotNil(expression: Any?, message: String)
+//- 값이 nil이 아닌지 확인합니다.
+//- expression: 테스트 대상 값
+//- message: 실패 시 출력할 메시지
+//1. XCTAssertEqualWithAccuracy(first: Double, second: Double, accuracy: Double, message: String)
+//- 두 실수 값이 지정한 정확도 범위 내에 있는지 확인합니다.
+//- first: 첫 번째 실수 값
+//- second: 두 번째 실수 값
+//- accuracy: 허용 오차 범위
+//- message: 실패 시 출력할 메시지
+//
+//등이 있습니다. 이외에도 다양한 Assert 메서드가 제공되며, 이를 이용해 테스트 케이스를 작성하고 실행할 수 있습니다.
